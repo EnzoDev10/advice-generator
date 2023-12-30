45b2b5e478807ca36d0b70534c9e5a38be6ebd75
Enzo Cañete <enzodev4@gmail.com>
2023-12-30 20:19:25 -0300

    btn animation update
    took out the rotation and applied prettier to both files.

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 // Runs an animation for the button when is clicked.
-// It's removed and added to ensure that the animation 
+// It's removed and added to ensure that the animation
 // runs every time that the button is clicked.
- 
-const btn = document.querySelector('#quote-btn');
 
-document.querySelector('#quote-btn').addEventListener('click', () => {
-  btn.classList.remove('btn-animation');
+const btn = document.querySelector("#quote-btn");
 
-  setTimeout(function(){
-    btn.classList.add('btn-animation');
-  },1000);
+document.querySelector("#quote-btn").addEventListener("click", () => {
+  btn.classList.remove("btn-animation");
+
+  setTimeout(function () {
+    btn.classList.add("btn-animation");
+  }, 1000);
 });
