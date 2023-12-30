a92c361d53dcf1f4637005682ded790a1f079c81
Enzo Cañete <enzodev4@gmail.com>
2023-12-30 19:25:43 -0300

    Btn click animation
    Added classes and a JS file for the animation

---
+++ main.js
@@ -0,0 +1,13 @@
+// Runs an animation for the button when is clicked.
+// It's removed and added to ensure that the animation 
+// runs every time that the button is clicked.
+ 
+const btn = document.querySelector('#quote-btn');
+
+document.querySelector('#quote-btn').addEventListener('click', () => {
+  btn.classList.remove('btn-animation');
+
+  setTimeout(function(){
+    btn.classList.add('btn-animation');
+  },1000);
+});
