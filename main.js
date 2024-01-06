24307953b81e2b5e7faeb8c2326de98266279257
Enzo Cañete <enzodev4@gmail.com>
2024-01-06 19:27:32 -0300

    Get advice from API
    made with the fetch function from JS

--- main.js
+++ main.js
@@ -1,10 +1,30 @@
+const btn = document.getElementById("quote-btn");
+
+window.onload = getRandomAdvice();
+
+async function getRandomAdvice() {
+  const apiUrl = "https://api.adviceslip.com/advice";
+
+  let response = await fetch(apiUrl);
+  if (response.ok) {
+    let json = await response.json();
+    // Gets the content out of the API to a json file.
+    let slip = json.slip;
+
+    // Then puts the respective values inside two different variables.
+    let id = slip.id;
+    let advice = slip.advice;
+
+  } else {
+    alert("something went wrong.");
+  }
+}
+
 // Runs an animation for the button when is clicked.
 // It's removed and added to ensure that the animation
 // runs every time that the button is clicked.
 
-const btn = document.querySelector("#quote-btn");
-
-document.querySelector("#quote-btn").addEventListener("click", () => {
+/* btn.addEventListener("click", () => {
   btn.classList.remove("btn-animation");
 
   setTimeout(function () {
@@ -12,9 +32,4 @@ document.querySelector("#quote-btn").addEventListener("click", () => {
   }, 1000);
 });
 
-btn.onclick = function () {
-  fetch('/get_advice', {
-      method: 'GET',
-      
-  })
-};
+ */
